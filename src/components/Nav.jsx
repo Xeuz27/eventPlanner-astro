73603859ed3699b614b436b80acbb4a31955e16e
Xeuz27 <jesusg.1301.jg@gmail.com>
2024-04-08 11:29:34 -0400

    feat: navbar now receives props to change its color and adapt the links dinamically with those props too

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,20 @@ import { navLinks } from '../data/siteConfig';
 import logo from '@assets/Edit_01.webp';
 const navMobile = '/*TODO: CREAR NAVMOBILE CLASSES CON TERNARIAS Y CONSTANTES*/';
 
-const Nav = () => {
+const Nav = ({ bgColor, btnHighlightClass }) => {
+	const bgColors = {
+		primary: 'bg-primary',
+		secondary: 'bg-neutral-300'
+	};
+	const linkClasses = {
+		primary: 'text-white text-opacity-70 hover:text-opacity-100 hover:bg-white hover:bg-opacity-20',
+		secondary: 'text-primary hover:text-white hover:bg-primary hover:bg-opacity-30'
+	};
+	const btnHighlightClasses = {
+		primary: 'bg-white text-primary bg-opacity-80 hover:bg-opacity-20 hover:text-white',
+		secondary:
+			'bg-primary text-white text-opacity-70 bg-opacity-80  hover:text-white hover:text-opacity-100 hover:bg-primary hover:bg-opacity-30'
+	};
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
 	const menuLeft = isMenuOpen ? 'left-0' : 'left-[-100%]';
 	const menuBg = isMenuOpen ? 'bg-white' : 'bg-white';
@@ -27,7 +40,7 @@ const Nav = () => {
 	);
 	return (
 		<header
-			className={`fixed bottom-0 z-30 max-md:border-r xs:w-[300px] md:border-b md:border-stone-400 ${menuLeft} top-0 flex flex-col items-center gap-2 bg-primary px-8 py-4 transition-all duration-500 md:inset-auto md:w-full md:flex-row md:justify-between md:px-4`}
+			className={`fixed bottom-0 z-30 max-md:border-r xs:w-[300px] md:border-b md:border-stone-400 ${menuLeft} top-0 flex flex-col items-center gap-2 ${bgColors[bgColor]} px-8 py-4 transition-all duration-500 md:inset-auto md:w-full md:flex-row md:justify-between md:px-4`}
 		>
 			<div className="flex w-full flex-row-reverse items-center justify-between md:w-auto">
 				<div className="h-[36px] w-[57px] overflow-hidden">
@@ -44,13 +57,13 @@ const Nav = () => {
 			<nav className="mx-auto">
 				<ul className="flex flex-col items-center gap-4 md:flex-row">
 					{navLinks.map(({ display, path, highlight = false }) => {
-						const classes = `text-white text-opacity-50 text-lg flex font-semibold tracking-wide rounded-full py-1 px-4 hover:text-opacity-100 transition-colors duration-300`;
+						const classes = `text-lg flex font-semibold tracking-wide rounded-full py-1 px-4 transition-colors duration-300`;
 						const afterActiveLink =
 							"after:bg-white after:absolute after:-top-4 after:hidden after:h-2 after:w-full after:content-[''] after:hover:block";
 						return (
 							<li key={path} className="relative">
 								<a
-									className={`${classes} ${highlight && 'bg-white bg-opacity-80  text-black text-opacity-95 hover:bg-opacity-20 hover:text-white hover:text-opacity-100'}`}
+									className={`${classes} ${highlight ? btnHighlightClasses[btnHighlightClass] : `${linkClasses[bgColor]}`}`}
 									href={path}
 								>
 									{display}
